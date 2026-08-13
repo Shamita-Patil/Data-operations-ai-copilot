65d46d10d9bd6293a73d9073bc9abfff25ec37ae
Shamita-Patil <shamita412@gmail.com>
2026-08-13 12:56:05 +0530

    Use configured Redis URL

--- backend/app/cache/redis.py
+++ backend/app/cache/redis.py
@@ -3,10 +3,8 @@ import redis
 from backend.app.core.config import settings
 
 
-redis_client = redis.Redis(
-    host="redis",
-    port=6379,
-    db=0,
+redis_client = redis.from_url(
+    settings.redis_url,
     decode_responses=True,
 )
 
